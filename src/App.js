8932e05768c74e3f7475192ef82267d656218a33
alexo <alexola.22@gmail.com>
2020-02-17 17:18:50 +0100

    Router install and getting the new id url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Provider} from 'react-redux';
+import {HashRouter as Router} from 'react-router-dom';
 import './App.css';
 
 import Navbar from './components/layout/Navbar';
@@ -13,9 +14,13 @@ class App extends Component {
   render(){
     return( 
       <Provider store={store}>
-          <Navbar /> 
-          <Landing />
-          <Footer /> 
+        <Router>
+          <div>
+            <Navbar /> 
+            <Landing />
+            <Footer /> 
+          </div>
+        </Router> 
       </Provider>    
       );
   }
