522902a88ab4dbb452f834ee8f5b81b03645105b
alexo <alexola.22@gmail.com>
2020-02-19 17:59:21 +0100

    Updates

---
+++ precache-manifest.e73c3a46f490d698fd368048b8d96120.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3e3a920f0fa0534a291fa33bf742523",
+    "url": "/womovie/index.html"
+  },
+  {
+    "revision": "f190f1fb31e9a3038176",
+    "url": "/womovie/static/css/main.51a49779.chunk.css"
+  },
+  {
+    "revision": "0db89f0933fc1b09c11f",
+    "url": "/womovie/static/js/2.7a69a1b3.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/womovie/static/js/2.7a69a1b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f190f1fb31e9a3038176",
+    "url": "/womovie/static/js/main.876a0474.chunk.js"
+  },
+  {
+    "revision": "44918c57f6a0d22209dd",
+    "url": "/womovie/static/js/runtime-main.3870745b.js"
+  }
+]);
